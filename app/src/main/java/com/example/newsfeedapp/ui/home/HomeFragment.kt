de49086ffbd41cc6f1a63be816bbf6ab231de6b2
ali <alisamymohamed@yahoo.com>
2020-05-13 22:31:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/newsfeedapp/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.newsfeedapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.newsfeedapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+}
