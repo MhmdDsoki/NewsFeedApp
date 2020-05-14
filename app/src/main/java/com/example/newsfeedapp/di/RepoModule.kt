0d2148aecc761589586061dcff169a232668142b
ali <alisamymohamed@yahoo.com>
2020-05-13 23:04:24 -0700

    setup NewsRepository with Koin (repo Module )

--- app/src/main/java/com/example/newsfeedapp/di/RepoModule.kt
+++ app/src/main/java/com/example/newsfeedapp/di/RepoModule.kt
@@ -2,11 +2,13 @@ package  com.example.newsfeedapp.di
 
 
 
+import com.example.newsfeedapp.data.NewsRepository
 import org.koin.dsl.module
 
 val repo = module {
 
-
+       // Provide NewsRepository
+    single { NewsRepository (get(),get()) }
 
 
 }
