8c9d63c5b9104f31fe64501c742701d83dcc3548
ali <alisamymohamed@yahoo.com>
2020-06-13 05:12:16 -0700

    added ui testing

--- app/src/androidTest/java/com/example/newsfeedapp/ui/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/example/newsfeedapp/ui/HomeFragmentTest.kt
@@ -1,7 +1,6 @@
 package com.example.newsfeedapp.ui
 
 
-
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import android.view.View
@@ -35,121 +34,185 @@ class HomeFragmentTest {
 
     @Test
     fun homeFragmentTest() {
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-Thread.sleep(700)
-        
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
         val recyclerView = onView(
-allOf(withId(R.id.newsRecycler),
-childAtPosition(
-withId(R.id.swipeRefresh),
-0)))
+            allOf(
+                withId(R.id.newsRecycler),
+                childAtPosition(
+                    withId(R.id.swipeRefresh),
+                    0
+                )
+            )
+        )
         recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-        
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-Thread.sleep(700)
-        
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
         pressBack()
-        
+
         val recyclerView2 = onView(
-allOf(withId(R.id.newsRecycler),
-childAtPosition(
-withId(R.id.swipeRefresh),
-0)))
+            allOf(
+                withId(R.id.newsRecycler),
+                childAtPosition(
+                    withId(R.id.swipeRefresh),
+                    0
+                )
+            )
+        )
         recyclerView2.perform(actionOnItemAtPosition<ViewHolder>(1, click()))
-        
+
         pressBack()
-        
+
         val recyclerView3 = onView(
-allOf(withId(R.id.newsRecycler),
-childAtPosition(
-withId(R.id.swipeRefresh),
-0)))
+            allOf(
+                withId(R.id.newsRecycler),
+                childAtPosition(
+                    withId(R.id.swipeRefresh),
+                    0
+                )
+            )
+        )
         recyclerView3.perform(actionOnItemAtPosition<ViewHolder>(8, click()))
-        
+
         pressBack()
-        
+
         val appCompatImageView = onView(
-allOf(withClassName(`is`("androidx.appcompat.widget.AppCompatImageView")), withContentDescription("Search"),
-childAtPosition(
-allOf(withClassName(`is`("android.widget.LinearLayout")),
-childAtPosition(
-withId(R.id.action_search),
-0)),
-1),
-isDisplayed()))
+            allOf(
+                withClassName(`is`("androidx.appcompat.widget.AppCompatImageView")),
+                withContentDescription("Search"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        childAtPosition(
+                            withId(R.id.action_search),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatImageView.perform(click())
-        
+
         val searchAutoComplete = onView(
-allOf(withClassName(`is`("android.widget.SearchView.SearchAutoComplete")),
-childAtPosition(
-allOf(withClassName(`is`("android.widget.LinearLayout")),
-childAtPosition(
-withClassName(`is`("android.widget.LinearLayout")),
-1)),
-0),
-isDisplayed()))
+            allOf(
+                withClassName(`is`("android.widget.SearchView.SearchAutoComplete")),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         searchAutoComplete.perform(replaceText("if"), closeSoftKeyboard())
-        
+
         val appCompatImageView2 = onView(
-allOf(withClassName(`is`("androidx.appcompat.widget.AppCompatImageView")), withContentDescription("Clear query"),
-childAtPosition(
-allOf(withClassName(`is`("android.widget.LinearLayout")),
-childAtPosition(
-withClassName(`is`("android.widget.LinearLayout")),
-1)),
-1),
-isDisplayed()))
+            allOf(
+                withClassName(`is`("androidx.appcompat.widget.AppCompatImageView")),
+                withContentDescription("Clear query"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatImageView2.perform(click())
-        
+
         val appCompatImageButton = onView(
-allOf(withContentDescription("Open navigation drawer"),
-childAtPosition(
-allOf(withId(R.id.toolbar),
-childAtPosition(
-withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
-0)),
-1),
-isDisplayed()))
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatImageButton.perform(click())
-        
+
         val navigationMenuItemView = onView(
-allOf(childAtPosition(
-allOf(withId(R.id.design_navigation_view),
-childAtPosition(
-withId(R.id.nav_view),
-0)),
-4),
-isDisplayed()))
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
         navigationMenuItemView.perform(click())
-        
+
         val appCompatImageButton2 = onView(
-allOf(withContentDescription("Navigate up"),
-childAtPosition(
-allOf(withId(R.id.toolbar),
-childAtPosition(
-withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
-0)),
-1),
-isDisplayed()))
+            allOf(
+                withContentDescription("Navigate up"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatImageButton2.perform(click())
-        
+
         val navigationMenuItemView2 = onView(
-allOf(childAtPosition(
-allOf(withId(R.id.design_navigation_view),
-childAtPosition(
-withId(R.id.nav_view),
-0)),
-1),
-isDisplayed()))
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         navigationMenuItemView2.perform(click())
-        }
-    
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -164,4 +227,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
